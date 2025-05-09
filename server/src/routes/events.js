fdc70e18806b4eda567946f9b542d6e57f6cbfe4
Ayush Bhattarai <ayush.scb@gmail.com>
2025-05-09 07:14:57 +0530

    Create events.js

---
+++ server/src/routes/events.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/Event');
+
+// Get all events
+router.get('/', async (req, res) => {
+  try {
+    const events = await Event.find().sort({ createdAt: -1 });
+    res.json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create new event
+router.post('/', async (req, res) => {
+  const event = new Event({
+    title: req.body.title,
+    time: req.body.time,
+    duration: req.body.duration
+  });
+
+  try {
+    const newEvent = await event.save();
+    res.status(201).json(newEvent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
